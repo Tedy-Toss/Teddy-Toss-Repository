df0bba175fb10675cb574b7aecd1e341fa25e437
superbird29 <aaron.tweden2014@gmail.com>
2020-02-18 11:04:07 -0600

    updates

---
+++ Assets/LevelNext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelNext : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            //laods scence 1
+            SceneManager.LoadScene(index);
+
+            //laod level with scene name
+            // SceneManager.LoadScene("scene 2");
+
+        }
+    }
+}
