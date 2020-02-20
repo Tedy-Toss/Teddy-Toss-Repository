d429914b023aa46a19c84802b99f6e0c740b6d0e
superbird29 <aaron.tweden2014@gmail.com>
2020-02-20 10:33:25 -0600

    updates

--- Assets/CompleteCameraController.cs
+++ Assets/CompleteCameraController.cs
@@ -3,25 +3,21 @@ using System.Collections;
 
 public class CompleteCameraController : MonoBehaviour {
 
-	public GameObject player;		//Public variable to store a reference to the player game object
+    public Transform Follow; //Public variable to store a reference to the player game object
 
+    public float smoothSpeed = 10f;
 
-	private Vector3 offset;			//Private variable to store the offset distance between the player and camera
-
-    public float yOffset = 3f;
-
-	// Use this for initialization
-	void Start () 
-	{
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
-    }
+	public Vector3 offset;			//Private variable to store the offset distance between the player and camera
 	
 	// LateUpdate is called after Update each frame
 	void LateUpdate () 
 	{
-		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-		transform.position = player.transform.position + offset;
-        transform.position = new Vector3(transform.position.x, transform.position.y + yOffset, transform.position.z);
+        Vector3 desiredLocation = Follow.position + offset;
+
+        Vector3 smoothedLocation = Vector3.Lerp(transform.position, desiredLocation, smoothSpeed * Time.deltaTime);
+
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = smoothedLocation;
+      
     }
 }
---
+++ Assets/MovingTerrain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTerrain : MonoBehaviour
+{
+    public float blockSpeed = 1f;
+    private Rigidbody2D MovingBlock;
+    public float Track = 0f;
+
+    private void Awake()
+    {
+        MovingBlock = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //MovingBlock.AddForce(transform.up * blockSpeed * Time.fixedDeltaTime);
+
+        MovingBlock.AddForce(transform.up * -blockSpeed * Time.fixedDeltaTime);
+    }
+}
