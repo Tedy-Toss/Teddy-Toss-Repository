ad9bf681b942ca7476eeea40c9155062af9fb49c
thomastyl <60270930+thomastyl@users.noreply.github.com>
2020-03-10 17:10:58 -0500

    First Commit
    block moves up and down at certain speed and bounds

---
+++ raycastDebug/Assets/ExampleMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int maxY = 8;
+    int minY = -8;
+    int speed = 2;
+    int direction = 1;
+    void Start()
+    {
+        StartCoroutine("moveBlock");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator moveBlock()
+    {
+        
+        while (true)
+        {
+            if(transform.position.y >= maxY)
+            {
+                direction = -1;
+                Debug.Log("in if " + transform.position.y);
+                
+            }
+            else if(transform.position.y <= minY)
+            {
+                direction = 1;
+                Debug.Log("in else " + transform.position.y);
+            }
+            
+            transform.Translate(0, (speed * Time.deltaTime) * direction, 0);
+         
+            yield return null;
+        }
+    }
+        
+ 
+}
